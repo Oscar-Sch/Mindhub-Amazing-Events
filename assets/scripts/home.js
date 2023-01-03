2b723ba812e475a3519706dc963332e2513cb8d3
Oscar-Sch <oscarschwerdt@gmail.com>
2023-01-03 09:36:23 -0300

    starting with filter

--- assets/scripts/home.js
+++ assets/scripts/home.js
@@ -1,21 +1,70 @@
+const data={};
+const cardsContainer= document.querySelector(".cards-container");
+const searchbar= document.querySelector(".filter-searchbar input");
+const searchBtn= document.querySelector(".filter-searchbar button");
+const checkBtns= document.querySelectorAll(".btn-check");
+const checkGroup= document.querySelector(".btn-group");
 //creo una funcion asincrona que se auto-instancia al comienzo de la ejecucion del script
 //para que traiga los datos desde el .json y mandarlos a la funcion RenderCards
-const LoadData=(async()=>{
-    const cardsContainer= document.querySelector(".cards-container");
-    let data;
+const LoadData=(async(data,container)=>{ 
     await fetch("./assets/data/data.json", {method:"GET"})
         .then(res=>res.json())
         .then(res=>{
-            data=res;
+            data.events=res.events;
+            data.currentDate=res.currentDate;
         });
-    RenderCards(cardsContainer,data)
-})();
+        RenderCards(container,data.events)
+})(data,cardsContainer);
+
+
+
+function ParseCategories(catStr){
+
+    return catStr.toLowerCase().split(",").map(cat=>cat.trim());
+
+}
+
+function ApplyFilter(cats,container,data){
+    let filtered=[];
+    filtered=data.filter(e=>{
+        for(let cat of cats){
+            if (e.category.toLowerCase()===cat){
+                return true;
+            }
+        }
+    });
+    if (filtered.length===0){
+        if(cats.length>1 || cats[0]!==""){
+            filtered=[]
+        }else{
+            filtered=data;
+        }
+    }
+    RenderCards(container,filtered);
+}
+
+function FilterObserver(container,data){
+    const searchbar= document.querySelector(".filter-searchbar input");
+    const searchBtn= document.querySelector(".filter-searchbar button");
+    const checkBtns= document.querySelectorAll(".btn-check");
+    let searchCategories=[];
+    searchBtn.addEventListener("click",()=>{
+        searchCategories= ParseCategories(searchbar.value);
+        ApplyFilter(searchCategories,container,data);
+    });
+    checkBtns.forEach(btn=>{
+        btn.addEventListener("click",()=>{
+            
+        })
+    })
+}
+
 
 
 //recibe un container padre y los datos de un evento para insertar un 
 //template dinamico en el interior del contenedor
-function LoadCard(container,event){
-    container.innerHTML+=
+function LoadCard(event){
+    return(
     `<article class="card col-10 col-md-5 col-lg-3">
         <img class="card-img-top" src="${event.image}" alt="a ${event.name} image">
         <div class="card-body">
@@ -26,14 +75,26 @@ function LoadCard(container,event){
                 <a href="./details.html" class="btn btn-see-more">See more!</a>
             </div>
         </div>
-    </article>`
-    ;
+    </article>`);
 }
+
+function RenderError(container){
+    let errorMsg=`<h2 class="not-found">Unexistent categories</h2><h3 class="not-found">¡Remember to use commas to separate the categories!</h3>`;
+    container.innerHTML=errorMsg;
+}
+
+
 //recibe un contenedor padre y el objeto extraido del .json,
 //y por cada evento manda a cargar una carta
 function RenderCards(container,data){
-        data.events.forEach(event => {
-            LoadCard(container,event);
-        });
+    let htmlStr=""; 
+    data.forEach(event => {
+        htmlStr+=LoadCard(event);
+    });
+    if(htmlStr){
+        container.innerHTML=htmlStr;
+    }else{
+        RenderError(container);
+    }
 }
 
